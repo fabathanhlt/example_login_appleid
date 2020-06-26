cbc20746cb4e448f236d3f6c09fbcc72c73034e9
Tung <nttung91@gmail.com>
2020-06-26 14:11:52 +0700

    -Update code sign-in Firebase with appleId by swift

---
+++ ios_example/SampleAppleSignIn/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  SampleAppleSignIn
+//
+//  Created by admin on 6/25/20.
+//  Copyright © 2020 y4gamilight. All rights reserved.
+//
+
+import UIKit
+import CoreUI
+
+class MainViewController: Y4gViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
